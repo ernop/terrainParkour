f6dee30343e50cd9f789682eb3bcc5f79456d915
Ernie <ernestfrench@gmail.com>
2017-05-16 00:00:57 -0700

    bump

--- terrain/terrain/settings.py
+++ terrain/terrain/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
+from localsettings import *
+
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -19,8 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'x!1^z0v#hm!wz(hb3&zy=*e#04a-o^r=iw(%i76i@#ac!xa%nb'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
