b3fe6dd250dfba2c8fba76ec69b83388dbe84a38
Ernie <ernestfrench@gmail.com>
2019-07-03 17:43:10 -0700

    localhost

--- image/terrainparkour/terrainparkour/settings.py
+++ image/terrainparkour/terrainparkour/settings.py
@@ -27,8 +27,8 @@ SECRET_KEY = 'klr$0tz*m*!%__mdb_i*=#7hbr#b-vd72tda%*jhjav@u-zp@#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['web',]
-ALLOW_HOSTS = ['web',]
+ALLOWED_HOSTS = ['web','localhost',]
+ALLOW_HOSTS = ['web','localhost',]
 
 
 # Application definition
